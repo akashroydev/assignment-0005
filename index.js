dcb18f37fb85cd638e2b2da28314c79d75f16bbf
akashroydev <akashro872923@gmail.com>
2025-08-29 20:35:35 +0600

    last commit

--- index.js
+++ index.js
@@ -57,23 +57,3 @@ for (const button of buttons) {
 
 
 
-
-//document.addEventListener("DOMContentLoaded", function (){
-//    const buttons = document.querySelectorAll(".call-btn-click");
-//    const outPut = document.getElementById("output");
-//
-//    for (const button of buttons) {
-//        button.addEventListener("click", function () {
-//            const img = parentDiv.querySelector("img").src;
-//            const parentDiv = this.closest(".call-btn-click");
-//            const header = parentDiv.querySelector("h2").textContent;
-//            const numBer = document.getElementById("number")
-//            const time = new Date().toLocaleTimeString();
-
-            
-//        });
-//    }
-//});
-
-
-
